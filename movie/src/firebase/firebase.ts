45944e51aa87247406737ce908aa1daf76a8b027
Stanislav588 <staskornienko01@gmail.com>
2025-01-02 12:26:40 +0100

    #

--- movie/src/firebase/firebase.ts
+++ movie/src/firebase/firebase.ts
@@ -6,6 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
+
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
