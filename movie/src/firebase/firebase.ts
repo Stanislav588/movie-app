8df0af1b904a04211258268e10824f29595bd8fa
Stanislav588 <staskornienko01@gmail.com>
2025-01-02 12:22:42 +0100

    #

--- movie/src/firebase/firebase.ts
+++ movie/src/firebase/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "movies-app-883a8.firebaseapp.com",
-  projectId: "movies-app-883a8",
-  storageBucket: "movies-app-883a8.firebasestorage.app",
-  messagingSenderId: "307789475110",
-  appId: "1:307789475110:web:7f6f615ac3df0ace2b65e7",
-  measurementId: "G-HVTRECWNVV",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
